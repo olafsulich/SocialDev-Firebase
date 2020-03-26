d29bf8bb139f9938a3e79886af0c62479ba4a685
Macbook <macbook@macbook-pro-macbook.home>
2020-03-26 23:15:55 +0100

    Switch react hook form to Formik

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Heading from '../components/atoms/Heading/Heading';
 import Input from '../components/atoms/Input/Input';
