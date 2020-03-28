ababaee0891bec0fa3a8819941449a5bf6305283
Macbook <macbook@macbook-pro-macbook.home>
2020-03-28 01:29:34 +0100

    Add correct types to useUpdate

--- src/hooks/useUpdate.tsx
+++ src/hooks/useUpdate.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 
-const useUpdate = (ref: any, value: {}, dependency?: never[]) => {
+const useUpdate = (
+  ref: firebase.firestore.DocumentReference,
+  value: number,
+  dependency?: object | null,
+) => {
   useEffect(() => {
     ref.update({ comments: value });
   }, [dependency]);
